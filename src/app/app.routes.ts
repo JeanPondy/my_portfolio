10ed4dd9850740e43f3c95328338f0f532812903
Jean Pondy <manbayesse@gmail.com>
2024-10-24 13:44:08 +0200

    app-config.ts updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     
 ];
+
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
