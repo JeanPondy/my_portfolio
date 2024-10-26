cd6686823edde2a69f79f2fef891bc4b0054ce4a
Jean Pondy <manbayesse@gmail.com>
2024-10-26 15:19:33 +0200

    bg updated

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
+  
+  constructor(private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    // Intersection Observer für Scroll-Effekte
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-visible");
+        } else {
+          entry.target.classList.remove("is-visible");
+        }
+      });
+    });
+
+    // Überwachen des Elements mit der ID #about-me
+    const aboutMeSection = this.el.nativeElement.querySelector("#about-me");
+    if (aboutMeSection) {
+      observer.observe(aboutMeSection);
+    }
+  }
 }
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
@@ -7,7 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
-export class MySkillsComponent {
+export class MySkillsComponent implements AfterViewInit {
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit() {
+    // Intersection Observer für Scroll-Effekte
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-visible");
+        } else {
+          entry.target.classList.remove("is-visible");
+        }
+      });
+    });
+
+    // Überwachen des Elements mit der ID #about-me
+    const aboutMeSection = this.el.nativeElement.querySelector("#my-skills");
+    if (aboutMeSection) {
+      observer.observe(aboutMeSection);
+    }
+  }
+
+  /* ----------------------------- */
 
   activeSection: string = ''; // Variable, um den aktiven Abschnitt zu verfolgen
 
